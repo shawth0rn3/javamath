4476837afa55bd1cdf90075607e9b9cb789c7af9
Steve Hawthorne <shawth0rn3@gmail.com>
2012-01-03 00:48:24 -0500

    first commit

---
+++ javamath2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.math.*;
+
+public class javamath 
+{
+	//basic i/o done, time for math
+
+	public static void main(String[] args) 
+	{
+		double x, y;  
+		//double variable assignment for precision
+		//e.g. 10.1 is not a int...
+		
+		System.out.print("enter a number, x");
+		//prompt
+		Scanner in = new Scanner(System.in);
+		//open the input stream...
+		x = in.nextDouble();
+		//assign x..
+		
+		System.out.print("enter your second number, y");
+		y = in.nextDouble();		
+		//assign y..
+		
+		//System.out.print("you entered:\t"+x+"\t and"+y);
+		//standard debug statement
+		//    /t will add tabs to string output
+		
+		System.out.print("x * y\t"+(x*y));
+		System.out.print("\nx + y\t"+(x+y));
+		System.out.print("\nx - y\t"+(x-y));
+		System.out.print("\nx / y\t"+(x/y));
+		
+		//formatted output..
+		//sloppy but very quick and easy
+		
+		double z;
+		z = Math.sin(x);
+		System.out.print("\nsin("+x+")=\t"+z);
+		//cleaner output via variable assignment
+
+	}
+
+}
